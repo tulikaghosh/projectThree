2717662c55f4fd48065043a50722b5609645e27a
TULIKA GHOSH <tulikagetsmails@gmail.com>
2022-02-01 01:34:25 -0500

    Add files via upload

--- projectThree/Project-3 back-end-beta/project-3-back-end-main/src/main/resources/schema.sql
+++ projectThree/Project-3 back-end-beta/project-3-back-end-main/src/main/resources/schema.sql
@@ -93,3 +93,37 @@ discount_description VARCHAR,
 discount_percentage DECIMAL(100, 2),
 PRIMARY KEY(discount_id),
 FOREIGN KEY(product_id) REFERENCES product_details(product_id) ON DELETE CASCADE);
+
+DROP TABLE IF EXISTS reviews;
+
+CREATE TABLE reviews(
+    review_id INT GENERATED ALWAYS AS IDENTITY,
+    product_id INT,
+    user_id INT,
+    date DATE DEFAULT CURRENT_DATE,
+    title VARCHAR(100),
+    review VARCHAR,
+    rating INT,
+    PRIMARY KEY(review_id),
+    FOREIGN KEY(product_id) REFERENCES product_details(product_id),
+    FOREIGN KEY(user_id) REFERENCES user_details(user_id)
+);
+
+DROP TABLE IF EXISTS purchased_items;
+
+CREATE TABLE purchased_items(
+    item_id INT GENERATED ALWAYS AS IDENTITY,
+    product_id INT,
+    user_id INT,
+    cart_id INT,
+    transaction_id INT,
+    purchase_date DATE DEFAULT CURRENT_DATE,
+    item_qty INT,
+    purchase_cost DECIMAL(100, 2),
+    PRIMARY KEY(item_id),
+    FOREIGN KEY(product_id) REFERENCES product_details(product_id),
+    FOREIGN KEY(user_id) REFERENCES user_details(user_id),
+    FOREIGN KEY(transaction_id) REFERENCES transaction_details(transaction_id),
+    FOREIGN KEY(cart_id) REFERENCES cart_details(cart_id)
+);
+
