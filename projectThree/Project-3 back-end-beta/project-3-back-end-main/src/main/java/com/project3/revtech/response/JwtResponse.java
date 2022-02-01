4fa8412385e99ffcd377ec32c05b8f8845525626
TULIKA GHOSH <tulikagetsmails@gmail.com>
2022-02-01 01:49:29 -0500

    response updated

--- projectThree/Project-3 back-end-beta/project-3-back-end-main/src/main/java/com/project3/revtech/response/JwtResponse.java
+++ projectThree/Project-3 back-end-beta/project-3-back-end-main/src/main/java/com/project3/revtech/response/JwtResponse.java
@@ -26,21 +26,22 @@ public class JwtResponse {
 	    this.contact = contact;
 	  }
 
-	public String getToken() {
-		return token;
-	}
+	public String getAccessToken() {
+	    return token;
+	  }
 
-	public void setToken(String token) {
-		this.token = token;
-	}
+	  public void setAccessToken(String accessToken) {
+	    this.token = accessToken;
+	  }
 
-	public String getType() {
-		return type;
-	}
+	  public String getTokenType() {
+	    return type;
+	  }
 
-	public void setType(String type) {
-		this.type = type;
-	}
+	  public void setTokenType(String tokenType) {
+	    this.type = tokenType;
+	  }
+
 
 	public int getUser_id() {
 		return user_id;
