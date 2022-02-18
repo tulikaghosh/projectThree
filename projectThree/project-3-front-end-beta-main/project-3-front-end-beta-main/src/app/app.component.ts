4cc7a559abca241b5d48b44d34325856d8ee23e5
TULIKA GHOSH <tulikagetsmails@gmail.com>
2022-02-17 22:15:19 -0500

    component.ts updated

--- projectThree/project-3-front-end-beta-main/project-3-front-end-beta-main/src/app/app.component.ts
+++ projectThree/project-3-front-end-beta-main/project-3-front-end-beta-main/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { TokenStorageService } from './services/token-storage.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'rev-tech';
+  USER_KEY = 'auth-user';
+
+
+  constructor(private tokenstorage: TokenStorageService) {
+    if (window.sessionStorage.getItem(this.USER_KEY)!=null){
+     this.tokenstorage.isLoggedIn=true;
+    }   
+  }
 }
